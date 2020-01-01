a302cd02e74f27dd5618117e4d78a2482a1eb92f
Davoleo <davoleo2001@gmail.com>
2020-01-01 22:44:28 +0100

    Add Links for Linux and Mac platforms

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-var button = document.getElementById("download-button");
+var buttonWindows = document.getElementById("windows-download-button");
+var buttonMac = document.getElementById("mac-download-button");
+var buttonLinux = document.getElementById("linux-download-button");
+
 var counterParagraph = document.querySelector('footer p');
 
 function getLatestVersion() {
     fetch(`https://api.github.com/repos/Davoleo/Map-of-Zodiac/releases/latest`, {method: 'GET'})
     .then(res => res.json())
     .then(res => {
-        button.innerHTML = button.innerHTML + " " + res.tag_name;
-        button.setAttribute("href", res.assets[0].browser_download_url);
+
+        console.log(res);
+        
+        res.assets.forEach( asset => {
+
+            if (asset.name.indexOf(".WIN") != -1) {
+                buttonWindows.innerHTML = buttonWindows.innerHTML + "v" + res.tag_name;
+                buttonWindows.setAttribute("href", asset.browser_download_url);
+            }
+
+            if (asset.name.indexOf(".OSX") != -1) {
+                buttonMac.innerHTML = buttonMac.innerHTML + "v" + res.tag_name;
+                buttonMac.setAttribute("href", asset.browser_download_url);
+            }
+
+            if (asset.name.indexOf(".LIN") != -1) {
+                buttonLinux.innerHTML = buttonLinux.innerHTML + "v" + res.tag_name;
+                buttonLinux.setAttribute("href", asset.browser_download_url);
+            }
+
+        });
     })
 }
 
@@ -14,12 +36,12 @@ function getDownloadCount() {
     fetch(`https://api.github.com/repos/Davoleo/Map-of-Zodiac/releases`, {method: 'GET'})
     .then(response => response.json())
     .then(response => {
-
         let count = 0;
 
         response.forEach(release => {
-            console.log(release.assets[0].download_count);
-            count += release.assets[0].download_count;
+            release.assets.forEach( asset => {
+                count += asset.download_count;
+            });
         });
 
         counterParagraph.textContent = counterParagraph.textContent + " " + count;
